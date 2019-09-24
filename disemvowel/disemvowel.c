14b3ab4a54f5f46356bb2e52b71b7f6fb541b9ef
xion1547 <xion1547@morris.umn.edu>
2019-09-24 11:50:12 -0500

    Adds code so it will remove vowels from input strings Codes added: 	isval: counts all vowels so we can alloc the proper amount of memory for the new string 	disemvowel: where the magic happens

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,48 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "disemvowel.h"
+#include <stdio.h>
+
+
+int isval(char *str1) {
+ int len;
+ char *result;
+ int a;
+
+ a = strlen(str1);
+ len = 0;
+ result = str1;
+
+ for (int i = 0; i < a; i++){
+      if (result[i] == 'a' || result[i] == 'e' || result[i] == 'i' || result[i] == 'o' || result[i] == 'u' || result[i] == 'A' || result[i] == 'E' || result[i] == 'I' || result[i] == 'O' || result[i] == 'U') {
+           len++;
+     }
+ }
+
+ return len;
+}
 
 char *disemvowel(char *str) {
-  return (char*) "";
+ char *result;
+ char *newString;
+ int len;
+ int j;
+
+ len = strlen(str);
+ result = str;
+
+ newString = (char*) calloc(len-isval(result)+1, sizeof(char)); 
+
+
+ for (int i = 0; i < len; i++){
+      if (result[i] == 'a' || result[i] == 'e' || result[i] == 'i' || result[i] == 'o' || result[i] == 'u' || result[i] == 'A' || result[i] == 'E' || result[i] == 'I' || result[i] == 'O' || result[i] == 'U') {
+//	     result[i] = "";
+     } else {
+	     newString[j] = result[i];
+	     j++;
+     }
+ }
+  newString[len-isval(result)+1] = '\0';
+  return newString;
 }
