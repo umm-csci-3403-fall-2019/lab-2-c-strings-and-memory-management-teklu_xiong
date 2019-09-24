8dba5e00fbd4818e44759cc357aa26de8689bf0a
xion1547 <xion1547@morris.umn.edu>
2019-09-24 11:52:22 -0500

    Frees up some alloc memmory in main.c

--- disemvowel/main.c
+++ disemvowel/main.c
@@ -6,11 +6,15 @@
 int main(int argc, char *argv[]) {
   char *line;
   size_t size;
+  char *a;
   
   size = 100;
   line = (char*) malloc (size + 1);
-
+  a = disemvowel(line);
+  
   while (getline(&line, &size, stdin) > 0) {
-    printf("%s\n", disemvowel(line));
+    printf("%s\n", a);
   }
+  free(a);
+  free(line);
 }
